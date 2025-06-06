ab5c6cae2596ee9ebe0039575a810a031a3dbfd5
saraswathy <saraswathychandra88@gmail.com>
2025-06-06 13:55:26 +0200

    add product data

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,39 @@
 import express from "express";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import Product from "./models/products.model.js";
 dotenv.config();
 
 const app = express();
-dotenv.config();
-console.log(process.env.MONGO_URI);
-app.get("/products", (req, res) => {
-  res.send("Hello, World!");
+// Middleware to parse JSON bodies
+app.use(express.json());
+// Middleware to parse URL-encoded bodies
+app.post("/api/products", async (req, res) => {
+  const product = req.body;
+  if (!product.name || !product.price || !product.image) {
+    return res.status(400).json({
+      success: false,
+      message: "Name ,image and price are required",
+    });
+  }
+  const newProduct = new Product(product);
+  try {
+    await newProduct.save();
+    res.status(201).json({
+      success: true,
+      message: "Product created successfully",
+      data: newProduct,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+      error: error.message,
+    });
+  }
 });
+
 app.listen(5000, () => {
   console.log("Server is running on port 5000 ");
   connectDB();
 });
-//bqqtNAUHANKYHtnX
